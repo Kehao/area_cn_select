2cb1615bfd6b33263c7a2489050e3ba6dfeedcc3
kehao <kehao.qiu@gmail.com>
2013-07-18 16:08:31 +0800

    modified:   lib/area_select_cn/engine.rb

--- lib/area_select_cn.rb
+++ lib/area_select_cn.rb
@@ -1,14 +1,16 @@
+require "area_select_cn/engine"
+
 module AreaSelectCn
+  autoload :DistrictController, 'area_select_cn/district_controller'
+  autoload :Id,                 'area_select_cn/id'
+  autoload :Parser,             'area_select_cn/parser'
+  autoload :District,           'area_select_cn/district'
+  autoload :FormBuilder,        'area_select_cn/form_builder'
+  autoload :Helper,             'area_select_cn/helper'
+
   #def self.include_helper(scope)
   #  ActiveSupport.on_load(:action_controller) do
   #    include scope::Helper if defined?(scope::Helper)
   #  end
   #end
 end
-
-require "area_select_cn/district_controller"
-require "area_select_cn/engine"
-require "area_select_cn/id"
-require "area_select_cn/parser"
-require "area_select_cn/district"
-require "area_select_cn/form_builder"
