0bf2b322587a1d8718b4537839efc592522cff76
kehao <kehao.qiu@gmail.com>
2013-07-18 16:52:14 +0800

    modified:   lib/area_select_cn/form_builder.rb

--- lib/area_select_cn/form_builder.rb
+++ lib/area_select_cn/form_builder.rb
@@ -3,13 +3,11 @@ module AreaSelectCn
   class FormBuilder < ActionView::Helpers::FormBuilder  
     def area_select(attr,options = {})
       @template.render "area_select_cn/area_select", 
-        area_select_options(self.object.region_code).merge(:f => self,:attr=>attr)
+        area_select_options(self.object.public_send(attr)).merge(:f => self,:attr=>attr)
     end
 
     def area_select_options(region_code,opts={})
     options = {
-      :name => "region_code",
-      :text => "地区",
       :selected_province  => nil,
       :selected_city      => nil,
       :selected_district  => nil,
