4737c646c65c935ed7ca6c85a2a8ed1ce1198b30
kehao <kehao.qiu@gmail.com>
2013-07-19 15:54:24 +0800

    modified:   lib/area_select_cn/district_controller.rb

--- lib/area_select_cn/district_controller.rb
+++ lib/area_select_cn/district_controller.rb
@@ -2,16 +2,10 @@
 module AreaSelectCn
   class DistrictController < ActionController::Base
     layout nil
-
     # 返回树状结构的整个省份，城市，区域列表，
     # 避免选择地区时的多次请求 
     def index
       render :json => AreaSelectCn::District.list
     end
-
-    #def list
-    #  @result = District.list(params[:id])
-    #  render :json => @result
-    #end
   end
 end
