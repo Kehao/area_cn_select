c9a4bb9d17f0c94a8e70926623b57a3c5c7d01fc
jasl <jasl9187@hotmail.com>
2013-08-19 23:16:49 +0800

    refactor dev env

--- spec/dummy/app/assets/javascripts/application.js
+++ spec/dummy/app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 //= require jquery
 //= require jquery_ujs
-//= require area_select_cn/jquery.district-ul
+//= require bootstrap
 //= require select2
 //= require select2_locale_zh-CN
+//= require area_select_cn/jquery.district-ul
